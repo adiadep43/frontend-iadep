71ba785f2b52b3398487209f6ae2d388e7ec8a6b
adiadep43 <62850516+adiadep43@users.noreply.github.com>
2020-04-20 15:41:49 -0300

    create 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react';
+import Logo from './Logo';
+
+import '../global.css';
+
+
+
+import ContBar from './ContBar';
+import Navigate from './Navigate';
+
+export default function RadioPage(){
+    function toggle() {
+        var sec = document.getElementById('sec');
+        var nav = document.getElementById('navigation');
+        
+        sec.classList.toggle('active');
+        nav.classList.toggle('active');
+    }
+
+    
+
+    return(
+        <>
+        <section className="banner" id="sec">
+        <header>
+            <Logo/>
+            <div id="toggle" onClick={toggle}></div>
+        </header>
+        <div className="content">
+            <br/>
+            <br/>
+            <h2>404 Pagina Não Encontrada</h2>
+            <br/>
+            <br/>
+                
+        </div>
+    </section>
+    
+    <Navigate />
+        </>
+    );
+}
